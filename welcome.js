23aa1ffe82c194b6b4e27c1826998201a35492b1
JakeJames00223 <mohamedchelghoum.3am2@gmail.com>
2024-06-08 16:48:55 +0100

    2nd commit

--- welcome.js
+++ welcome.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
 const email = urlParams.get('email');
-document.write('<h1> your name is : ' + username + '</h1>');
-document.write('<h1> your email is : ' + email + '</h1>');
+document.write('<div class="wlcm"> your name is : ' + username + '</div>');
+document.write('<div class="wlcm"> your email is : ' + email + '</div>');
